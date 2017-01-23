d887a02e56050f7faa529b78987dcf40a34f54ef
Maic Lopez Saenz <maic@lopsae.com>
2017-01-23 10:32:40 -0800

    implement icon dragging

--- playgrounds/dragAndDrop.playground/Contents.swift
+++ playgrounds/dragAndDrop.playground/Contents.swift
@@ -4,13 +4,17 @@ import UIKit
 import PlaygroundSupport
 
 
-class TouchView: UIView {
+class DragAndDropView: UIView {
 
     private let touchesView: TouchDebugView
+    private let documentView, trashView: UIImageView
 
+    private var dragOffset: CGPoint?
 
     override init(frame: CGRect) {
         touchesView = TouchDebugView()
+        documentView = UIImageView()
+        trashView = UIImageView()
 
         super.init(frame: frame)
         backgroundColor = UIColor.orange
@@ -19,11 +23,11 @@ class TouchView: UIView {
         let documentCenter = CGPoint(x: bounds.width / 2, y: bounds.height / 4)
         let trashCenter = CGPoint(x: bounds.width / 2, y: bounds.height * 3 / 4)
 
-        let documentView = UIImageView(frame: CGRect(center: documentCenter, side: iconSide))
+        documentView.frame = CGRect(center: documentCenter, side: iconSide)
         documentView.contentMode = .scaleAspectFit
         documentView.image = UIImage(named: "file.png")
 
-        let trashView = UIImageView(frame: CGRect(center: trashCenter, side: iconSide))
+        trashView.frame = CGRect(center: trashCenter, side: iconSide)
         trashView.contentMode = .scaleAspectFit
         trashView.image = UIImage(named: "delete.png")
 
@@ -40,26 +44,40 @@ class TouchView: UIView {
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesView.touchPoints = touches.map({ $0.location(in: self) })
+
+        let touchLocation = touches.first!.location(in: self)
+        if (documentView.frame.contains(touchLocation)) {
+            dragOffset = documentView.frame.origin - touchLocation
+        }
+
         super.touchesBegan(touches, with: event)
     }
 
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesView.touchPoints = touches.map({ $0.location(in: self) })
+
+        if let dragOffset = dragOffset {
+            let touchLocation = touches.first!.location(in: self)
+            documentView.frame.origin = touchLocation + dragOffset
+        }
+
         super.touchesMoved(touches, with: event)
     }
 
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesView.touchPoints = []
+        dragOffset = nil
         super.touchesEnded(touches, with: event)
     }
 
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
         touchesView.touchPoints = []
+        dragOffset = nil
         super.touchesCancelled(touches, with: event)
     }
 
 }
 
-let view = TouchView(frame: CGRect(width: 300, height: 500))
+let liveView = DragAndDropView(frame: CGRect(width: 300, height: 500))
 
-PlaygroundPage.current.liveView = view
+PlaygroundPage.current.liveView = liveView
