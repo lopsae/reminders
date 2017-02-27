94f596b1504c71163563ef1b1930209953577b45
Maic Lopez Saenz <maic@lopsae.com>
2017-02-27 15:53:57 -0800

    add code for tabularecta for significant digits

---
+++ python/tabula_recta.py
@@ -0,0 +1,41 @@
+#! /usr/bin/env python3
+
+import re
+
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".lower()
+
+letters = []
+for index in range(len(alphabet)):
+	letters.append(alphabet[index])
+
+tabula = []
+for shift in range(len(letters)):
+	shiftedLetters = []
+	for index in range(len(letters)):
+		shiftedIndex = (shift + index) % len(letters)
+		letter = letters[shiftedIndex]
+		shiftedLetters.append(letter)
+
+	tabula.append(shiftedLetters)
+
+
+cypher = "tuccnimechlxbguhvpesyvbsuxihryccmcptwkxcfmbpemvjvhahxdwvqmbrfwfkkiiwbivplvogiyeelwalvjmvaewdiibeexbvrtotewrkecbxrfuukepjgjsfjkaxdmcztbafmnqfstfkbtnxkmssurna"
+
+keys = "Any attempts to sabotage, disrupt, delay, repurpose, alter, or otherwise interfere with the operations of the aforementioned transport, regardless of whether or not said sabotage, disruption, delay, repurposing, alteration, or interference is within the bounds of the signatory State or any other State, including both signatories and non-signatories, shall incur a penalty commensurate with the number of preceding penalties already incurred, according to the chart of Appendix B, and as determined by an impartial vote of representatives of all signatory States."
+
+keys = keys.lower()
+keys = re.sub("[^a-z]", "", keys)
+
+decodedLetters = []
+
+for index, letter in enumerate(cypher):
+	keyLetterIndex = letters.index(keys[index])
+	shiftedAlph = tabula[keyLetterIndex]
+
+	cypherLetterIndex = shiftedAlph.index(letter)
+	decodedLetters.append(letters[cypherLetterIndex])
+
+	# print(index, letter, keys[index], shiftedAlph[cypherLetterIndex], "".join(shiftedAlph))
+
+print("".join(decodedLetters))
+
