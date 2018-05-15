2d36b572154936219f4a15d54d51d49837f2020c
Maic Lopez Saenz <maic@lopsae.com>
2018-05-15 10:17:15 -0700

    remove rxSwift code from playground files
    + in ObjectMapperPlaygrounds

--- xcode/objectMapperPlaygrounds/Playgrounds.playground/Pages/Start.xcplaygroundpage/Contents.swift
+++ xcode/objectMapperPlaygrounds/Playgrounds.playground/Pages/Start.xcplaygroundpage/Contents.swift
@@ -3,13 +3,3 @@
 import RxSwift
 import RxSwiftPlaygrounds
 
-var str = "Hello, playground"
-Observable.just("Cosa").do(onNext: {
-  string in
-  check(actual: string, expected: "Cosa")
-  print(string)
-}).subscribe()
-
-
-print("👑 finis coronat opus~")
-
