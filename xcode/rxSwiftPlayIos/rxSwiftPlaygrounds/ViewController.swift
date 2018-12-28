a12d64cb784ca761e937aa111f645c82727243e6
Maic Lopez Saenz <maic@lopsae.com>
2018-12-27 16:48:46 -0800

    raw xcode project for RxSwit with iOS frameworks

---
+++ xcode/rxSwiftPlayIos/rxSwiftPlaygrounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rxSwiftPlaygrounds
+//
+//  Created by Maic Lopez Saenz on 12/27/18.
+//  Copyright © 2018 LopSae. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
