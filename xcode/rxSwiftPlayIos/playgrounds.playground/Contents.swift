894a3b60c3668e8f555f31521ba936d0a9a3fa8d
Maic Lopez Saenz <maic@lopsae.com>
2019-01-04 12:26:54 -0800

    add playground file with note that it doesnt work

---
+++ xcode/rxSwiftPlayIos/playgrounds.playground/Contents.swift
@@ -0,0 +1,9 @@
+
+
+// Creating a Cocoa Touch framework target does not properly import the pod
+// The app and framework may build, but upon using the pod there are errors
+// Suspect of this issue is cocoapods 1.5, sources pending
+
+
+var str = "Hello, playground"
+
