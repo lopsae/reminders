74b4c2b6091d643c096921b3ec07573f818b6a3b
Maic Lopez Saenz <maic@lopsae.com>
2023-12-01 15:58:03 -0800

    add additional tests for countAdyacentElements

--- playgrounds/interviewing/countAdyacentElements.playground/Contents.swift
+++ playgrounds/interviewing/countAdyacentElements.playground/Contents.swift
@@ -4,6 +4,7 @@
 // For [1, 2, 2, 3, 3, 3, 5, 5, 5, 5, 5]
 // Target `3` returns a count of 3
 
+
 func findStartBoundary(target: Int, array: [Int]) -> Int? {
     if (array.isEmpty) {
         return nil
@@ -44,13 +45,17 @@ func findStartBoundary(target: Int, array: [Int]) -> Int? {
     return nil
 }
 
-//           0  1  2  3  4  5  6  7  8  9  10 11 12 13 14  15
-let array = [1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 8, 8, 8, 8, 10, 10]
+//           0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
+let array = [1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 7, 7, 7, 7, 8]
 print("array.count: \(array.count)")
-print("find 2, expected 3:   \(findStartBoundary(target: 2, array: array))")
-print("find 4, expected nil: \(findStartBoundary(target: 4, array: array))")
-print("find 5, expected 7:   \(findStartBoundary(target: 5, array: array))")
-print("find 8, expected 10:  \(findStartBoundary(target: 8, array: array))")
+print("find 0, expected nil: \(findStartBoundary(target: 0, array: array).orNil)")
+print("find 1, expected 0:   \(findStartBoundary(target: 1, array: array).orNil)")
+print("find 2, expected 3:   \(findStartBoundary(target: 2, array: array).orNil)")
+print("find 4, expected nil: \(findStartBoundary(target: 4, array: array).orNil)")
+print("find 5, expected 7:   \(findStartBoundary(target: 5, array: array).orNil)")
+print("find 7, expected 10:  \(findStartBoundary(target: 8, array: array).orNil)")
+print("find 8, expected 14:  \(findStartBoundary(target: 8, array: array).orNil)")
+print("find 9, expected nil: \(findStartBoundary(target: 8, array: array).orNil)")
 
 
 //func foundEndBoundary(target: Int, array: [Int]) -> Int? {
