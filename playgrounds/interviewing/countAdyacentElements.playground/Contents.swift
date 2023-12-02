6d07bf96239b0bf99dfd07be7978b8921b655934
Maic Lopez Saenz <maic@lopsae.com>
2023-12-01 17:36:55 -0800

    use array slice for findStartBoundary

--- playgrounds/interviewing/countAdyacentElements.playground/Contents.swift
+++ playgrounds/interviewing/countAdyacentElements.playground/Contents.swift
@@ -5,65 +5,63 @@
 // Target `3` returns a count of 3
 
 
-func findStartBoundary<T: Comparable>(target: T, array: [T]) -> Int? {
+func findStartBoundary<T: Comparable>(target: T, array: [T]) -> [T].Index? {
+    return findStartBoundary(target: target, slice: array[...])
+}
+
+func findStartBoundary<T: Comparable>(target: T, slice: ArraySlice<T>) -> ArraySlice.Index? {
     let log = Logger(enabled: false)
-    log("▶️ target: \(target) checking: \(array)")
-    if (array.isEmpty) {
+    log("▶️ target: \(target) slice: \(slice) s/e: \(slice.startIndex)/\(slice.endIndex) c: \(slice.count)" )
+    if (slice.isEmpty) {
         log("⚙️ checking empty, returning nil")
         return nil
     }
-    if (array.count == 1) {
-        log("⚙️ checking single: \(array)")
-        if array[0] == target {
-            return 0
+    if (slice.count == 1) {
+        log("⚙️ checking single: \(slice)")
+        if slice.first == target {
+            return slice.startIndex
         } else {
             return nil
         }
     }
 
 
-    let middleIndex = array.count / 2
-    log("⚙️ middleIndex: \(middleIndex), value: \(array[middleIndex])")
+    let middleIndex = slice.startIndex + (slice.count / 2)
+    log("⚙️ middleIndex: \(middleIndex), value: \(slice[middleIndex])")
 
-    if array[middleIndex] == target {
-        if middleIndex == 0 {
+    if slice[middleIndex] == target {
+        if middleIndex == slice.startIndex {
             // First element is always boundary
-            return 0
+            log("⏹️ first element at: \(middleIndex)")
+            return slice.startIndex
         }
-        if array[middleIndex - 1] != target {
+        if slice[middleIndex - 1] != target {
             // Previous element is different, middle is boundary
+            log("⏹️ boundary element at: \(middleIndex)")
             return middleIndex
         }
 
         // Otherwise search at lower half
-        return findStartBoundary(target: target, array: Array(array[0..<middleIndex]))
+        return findStartBoundary(target: target, slice: slice[..<middleIndex])
 
-    } else if array[middleIndex] > target {
+    } else if slice[middleIndex] > target {
         // middle is larger, search lower half
-        return findStartBoundary(target: target, array: Array(array[0..<middleIndex]))
+        return findStartBoundary(target: target, slice: slice[..<middleIndex])
 
     } else {
         // middle is smaller, search higher half
-        let maybeFound = findStartBoundary(target: target, array: Array(array[middleIndex..<array.count]))
-        if let found = maybeFound {
-            // Adjust for array slice position
-            return middleIndex + found
-        }
+        return findStartBoundary(target: target, slice: slice[middleIndex...])
     }
-
-    // Not found
-    return nil
 }
 
 //            0  1  2  3  4  5  6  7  8  9  10 11 12 13 14
 let sArray = [1, 1, 1, 2, 2, 2, 2, 5, 5, 5, 7, 7, 7, 7, 8]
-print("array.count: \(sArray.count)")
 print("find 0, expected nil: \(findStartBoundary(target: 0, array: sArray).orNil)")
 print("find 1, expected 0:   \(findStartBoundary(target: 1, array: sArray).orNil)")
 print("find 2, expected 3:   \(findStartBoundary(target: 2, array: sArray).orNil)")
 print("find 4, expected nil: \(findStartBoundary(target: 4, array: sArray).orNil)")
 print("find 5, expected 7:   \(findStartBoundary(target: 5, array: sArray).orNil)")
-print("find 7, expected 10:  \(findStartBoundary(target: 8, array: sArray).orNil)")
+print("find 7, expected 10:  \(findStartBoundary(target: 7, array: sArray).orNil)")
 print("find 8, expected 14:  \(findStartBoundary(target: 8, array: sArray).orNil)")
 print("find 9, expected nil: \(findStartBoundary(target: 9, array: sArray).orNil)")
 
